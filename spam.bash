8d6ac53f8a745790d57fe010ae5683f4f013372a
Eric Matt <erichmatt@gmail.com>
2017-03-09 16:57:15 -0500

    added the file

---
+++ spam.bash
@@ -0,0 +1,6 @@
+COUNTER=0
+while [ $COUNTER -gt 0 ]; do
+	echo $COUNTER bottles of beer on the wall, $COUNTER bottles of beer, take one down and pass it around,| espeak
+	let COUNTER=COUNTER-1
+	echo $COUNTER bottles of beer on the wall | espeak
+done
