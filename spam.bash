be55fcd456d1429247bb813f203d7b105c7886e4
binarydinosaur252 <cyrusb252@gmail.com>
2017-03-11 10:36:50 -0500

    made 999 bottles.

--- spam.bash
+++ spam.bash
@@ -1,4 +1,4 @@
-COUNTER=99
+COUNTER=999
 while [ $COUNTER -gt 0 ]; do
 	echo $COUNTER bottles of beer on the wall, $COUNTER bottles of beer, take one down and pass it around,| espeak
 	let COUNTER=COUNTER-1
