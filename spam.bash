163bbf173a06dd7f9338c8ae559b45b025f7b5dc
Eric Matt <erichmatt@gmail.com>
2017-04-06 08:55:10 -0400

    nothing imporant

--- spam.bash
+++ spam.bash
@@ -2,5 +2,6 @@ COUNTER=99
 while [ $COUNTER -gt 0 ]; do
 	echo $COUNTER bottles of beer on the wall, $COUNTER bottles of beer, take one down and pass it around,| espeak
 	let COUNTER=COUNTER-1
+    touch $COUNTER
 	echo $COUNTER bottles of beer on the wall | espeak
 done
